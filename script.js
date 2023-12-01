7830dac64f07629fca7dde79b1ac5b00bce73e04
Peter Boyle <boyle.peter614@gmail.com>
2023-12-01 01:11:49 -0500

    updates

--- script.js
+++ script.js
@@ -2,27 +2,62 @@ const contentBox = document.getElementById('contentBox');
 const aboutContent = document.querySelectorAll(".about");
 const contactContent = document.querySelectorAll(".contact");
 const closeContentBtn = document.getElementById("closeContent");
+const tickerContainer = document.getElementById("ticker-container")
+const tickerText = document.getElementById("ticker-text");
+const key = `6bf1aec30e85477ca6ffa26db9adf3f6`;
+const endpoint = `https://newsapi.org/v2/everything?q=tech&apiKey=${key}`;
+let newsString = '';
+
+var width, containerwidth, left;
+
+const callForNews = async () => {
+    let articleCount = 0;
+    const news = await fetch(endpoint).then((res) => {
+        return res.json()
+    }).catch((e) => {
+        console.log(e)
+    });
+    console.log(news)
+    news.articles.forEach((article) => {
+        newsString += ` ${article.description.replace(/\s+/g,' ').trim()} - ${article.author.replace(/\s+/g,' ').trim()}  ::  `;
+        articleCount++
+        if (articleCount == news.articles.length){
+            populateNews(newsString);
+        }
+    });
+};
+callForNews();
+
+const populateNews = (news) => {
+    tickerText.innerText = news;
+    function scrollleft() {
+        tickerText.style.left = '100%';
+        setTimeout(scrollleft, 25);
+      }
+       scrollleft();
+
+};
 
 const populateAbout = () => {
     contentBox.classList.remove('hide');
-    aboutContent.forEach((element)=>{
+    aboutContent.forEach((element) => {
         element.classList.remove('hide');
     });
 };
 
 const populateContact = () => {
     contentBox.classList.remove('hide');
-    contactContent.forEach((element)=>{
+    contactContent.forEach((element) => {
         element.classList.remove('hide');
     });
 };
 
 const closeContentBox = () => {
     contentBox.classList.add('hide');
-    aboutContent.forEach((element)=>{
+    aboutContent.forEach((element) => {
         element.classList.add('hide');
     });
-    contactContent.forEach((element)=>{
+    contactContent.forEach((element) => {
         element.classList.add('hide');
     });
 };
